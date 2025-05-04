723d244a88354c06610e41d9b636d3012a347eed
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-05-04 13:31:22 +0200

    project

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,12 +1,17 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Sign Up page', () => {
-  before(() => {
+describe('Sign in flow', () => {
+  const email = 'testuser@example.com'; // lub dynamiczny: faker.internet.email()
+  const password = 'Test1234!';
 
-  });
+  it('should sign in with valid credentials', () => {
+    signInPage.visit();
+    signInPage.fillEmail(email);
+    signInPage.fillPassword(password);
+    signInPage.submit();
 
-  it('should ...', () => {
-
+    cy.contains('Your Feed').should('be.visible');
   });
 });
+
