6d74229366daf3510f0a4b6c2ce3a9e1a1a30fe8
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-05-17 16:58:46 +0200

    Add E2E tests with POM and Faker

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,17 +1,30 @@
+import { faker } from '@faker-js/faker';
+
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Sign in flow', () => {
+ HEAD
+describe('Sign in flow'), () => {
   const email = 'testuser@example.com';
   const password = 'Test1234!';
-
-  it('should sign in with valid credentials', () => {
+  const name = faker.person.firstName();
+  it('should sign in with valid credentials'), () => {
     signInPage.visit();
     signInPage.fillEmail(email);
     signInPage.fillPassword(password);
     signInPage.submit();
 
     cy.contains('Your Feed').should('be.visible');
+
+describe('Sign Up Test', () => {
+  it('should register a new user', () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+
+    cy.visit('/signup');
+    cy.get('[data-qa="email-input"]').type(email);
+    cy.get('[data-qa="password-input"]').type(password);
+    cy.get('[data-qa="submit-button"]').click();
   });
 });
 
